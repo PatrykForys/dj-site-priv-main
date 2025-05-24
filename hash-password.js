8e85ab63072a8419bf46fd3dd471660b64727d32
PatrykForys <150722488+PatrykForys@users.noreply.github.com>
2025-05-24 13:09:45 +0200

    a

--- hash-password.js
+++ hash-password.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs');
 
-const password = 'admin1';
+const password = 'adminnn';
 
 bcrypt.hash(password, 10).then(hash => {
     console.log('Zahashowane hasło:', hash);
 }); 
+
